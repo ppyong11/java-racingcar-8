1b7dbcf518909ca8b45c51eb9d3c5f6cb598462d
pponya11 <kah1722291@gmail.com>
2025-10-24 03:38:39 +0900

    feat: 알파벳 외 문자 입력 에러 코드 추가

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     DUPLICATED_NAME("자동차 이름은 중복될 수 없습니다."),
     INVALID_NAME_LENGTH("자동차 이름이 5자 이하여야 합니다."),
     EMPTY_NAME("자동차 이름은 비어 있을 수 없습니다."),
+    INVALID_CHARACTER("자동차 이름은 알파벳만 입력해야 합니다."),
     INVALID_COUNT("시도 횟수는 1 이상이어야 합니다.");
 
     private final String message;
