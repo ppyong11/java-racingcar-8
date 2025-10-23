74d4f2a232ff622eeb73de01c0646beb6dd2cee7
pponya11 <kah1722291@gmail.com>
2025-10-23 23:42:27 +0900

    feat(test): Input(), Output() 구현 및 Output() 테스트 작성

---
+++ src/main/java/view/IOView.java
@@ -0,0 +1,33 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import domain.Car;
+import domain.Cars;
+
+import java.util.List;
+
+public class IOView {
+
+    public static String inputNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        return Console.readLine();
+    }
+    public static int inputCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static void outputResultTitle() {
+        System.out.println("실행 결과");
+    }
+
+    public static void outputCars(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+    }
+
+    public static void outputWinner(String winner){
+        System.out.println("최종 우승자 : " + winner);
+    }
+}
---
+++ src/test/java/racingcar/IOViewTest.java
@@ -0,0 +1,54 @@
+package racingcar;
+
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import domain.Car;
+import domain.Cars;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import view.IOView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IOViewTest extends NsTest {
+    // 실제 출력을 가로채서 내용을 메모리에 저장
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() { // 테스트 시작 전
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() { // 테스트 종료 후
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 전진_출력_테스트() {
+        List<String> list = List.of("pobi", "woni");
+        Cars cars = new Cars(list);
+        cars.getCars().get(0).move(4);
+        cars.getCars().get(1).move(3);
+
+        IOView.outputCars(cars.getCars());
+
+        String result = outputStream.toString();
+        String[] lines = result.split(System.lineSeparator());
+
+        assertThat(lines[0]).isEqualTo("pobi : -");
+        assertThat(lines[1]).isEqualTo("woni : ");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
