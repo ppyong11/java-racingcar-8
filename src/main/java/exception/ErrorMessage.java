415b43e5664bc8cb144c12633c552e6d562a56bb
pponya11 <kah1722291@gmail.com>
2025-10-24 02:33:50 +0900

    feat: 에러 코드 및 메시지 작성

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package exception;
+
+public enum ErrorMessage {
+    UNSUPPORTED_DELIMITER("구분자는 쉼표(,)여야 합니다."),
+    DUPLICATED_NAME("자동차 이름은 중복될 수 없습니다."),
+    INVALID_NAME_LENGTH("자동차 이름이 5자 이하여야 합니다."),
+    EMPTY_NAME("자동차 이름은 비어 있을 수 없습니다."),
+    INVALID_COUNT("시도 횟수는 1 이상이어야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
