8bdd15d1a9e5ebbc81d4a3aa8e07757071c3e4a6
pponya11 <kah1722291@gmail.com>
2025-10-24 18:35:06 +0900

    feat: 입력 값 검증 후 Cars 객체 생성 로직 추가

---
+++ src/main/java/service/RacingcarService.java
@@ -0,0 +1,28 @@
+package service;
+
+import domain.Car;
+import domain.Cars;
+import service.util.NameParser;
+import service.util.RandomNumberGenerator;
+import service.validator.InputValidator;
+
+import java.util.List;
+
+public class RacingcarService {
+
+    public Cars makeCars(String inputName, int count) {
+        List<String> names = parseAndValidate(inputName, count);
+
+        return new Cars(names); // cars 객체 생성
+    }
+
+    private List<String> parseAndValidate(String inputName, int count) {
+        List<String> nameList = NameParser.nameParse(inputName);
+
+        InputValidator.validateName(nameList);
+        InputValidator.countValidator(count);
+
+        return nameList; // 검증에서 에러 안 터지면 반환
+    }
+
+}
