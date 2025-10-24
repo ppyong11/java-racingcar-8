68904f5f9fc7fd68357698070f401ba735cb913d
pponya11 <kah1722291@gmail.com>
2025-10-24 21:30:56 +0900

    refactor: try-catch 추가 (예외 발생 시 잡아서 다시 예외 던지기)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import controller.RacingcarContoller;
 import service.RacingcarService;
-import service.util.RandomNumberGenerator;
+import view.IOView;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,6 +10,14 @@ public class Application {
         RacingcarService racingcarService = new RacingcarService();
         RacingcarContoller racingcarContoller = new RacingcarContoller(racingcarService);
 
-        racingcarContoller.run();
+        try {
+            racingcarContoller.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException(e);
+        } finally {
+            IOView.close();
+        }
+
     }
 }
