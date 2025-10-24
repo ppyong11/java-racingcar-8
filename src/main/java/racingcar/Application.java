047e66857900143e0f91f0eba2560d987fb9da0d
pponya11 <kah1722291@gmail.com>
2025-10-24 20:24:03 +0900

    feat: main 동작 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import controller.RacingcarContoller;
+import service.RacingcarService;
+import service.util.RandomNumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingcarService racingcarService = new RacingcarService();
+        RacingcarContoller racingcarContoller = new RacingcarContoller(racingcarService);
+
+        racingcarContoller.run();
     }
 }
