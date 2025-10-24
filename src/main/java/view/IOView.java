8736154494ef2db04ac94b837b8eebf30f6905e1
pponya11 <kah1722291@gmail.com>
2025-10-24 21:37:13 +0900

    refactor: println 추가

--- src/main/java/view/IOView.java
+++ src/main/java/view/IOView.java
@@ -8,15 +8,17 @@ import java.util.List;
 public class IOView {
 
     public static String inputNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
-    public static int inputCount() {
+
+    public static String inputCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
     public static void outputResultTitle() {
+        System.out.println();
         System.out.println("실행 결과");
     }
 
@@ -24,9 +26,14 @@ public class IOView {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
+        System.out.println();
     }
 
     public static void outputWinner(String winner){
         System.out.println("최종 우승자 : " + winner);
+    }
+
+    public static void close() {
+        Console.close();
     }
 }
